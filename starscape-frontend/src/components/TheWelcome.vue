8304a47ff6d291bfd0d5ae740e584aa8e52ac3ec
ashikgligich <ashton.gligich@gmail.com>
2024-02-06 09:10:44 -0500

    e
    e

---
+++ starscape-frontend/src/components/TheWelcome.vue
@@ -0,0 +1,18 @@
+<template>
+    <div>
+      <h1 v-if="answer">Correct</h1>
+      <h1 v-else>Incorrect</h1>
+      <h2 v-for="animal in animals" :key="animal">{{ animal }}</h2>
+    </div>
+  </template>
+  
+  <script setup>
+  const answer = true;
+  const animals = ["Ape", "Baboon", "elk", "Joe Mama"];
+  </script>
+  
+  <style scoped>
+  h1 {
+    background-color: red;
+  }
+  </style>
