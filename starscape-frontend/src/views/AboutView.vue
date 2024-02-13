3c4b497562ecb2c1912aff83dcdaed460c8a0598
ashikgligich <144455072+ashikgligich@users.noreply.github.com>
2024-02-13 13:18:26 -0500

    e
    e

--- starscape-frontend/src/views/AboutView.vue
+++ starscape-frontend/src/views/AboutView.vue
@@ -1,6 +1,7 @@
 <template>
   <div class="about">
-    <h1>This is an about page</h1>
+    <h1>This is an about page</h1>\
+    <h4></h4>
   </div>
   <main>
     <MainModel />
@@ -27,6 +28,16 @@ fetch(url)
 .then(json => {
   // Do something with the data
   console.log(json.ships);
+  let ships = json.ships;
+
 });
+//output the array of ships to html page  
+    
+// ships.forEach(ship => {
+//   let card = document.createElement('div');
+//   card.className = 'card';
+//   card.innerHTML = ship.name;
+//   document.body.appendChild(card);
+// });
 
 </script>
