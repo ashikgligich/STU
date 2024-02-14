6b05f550ef1677fa3d90c8f80e2daff42b39cff6
LeoDwi <leod13579@gmail.com>
2024-02-13 20:27:39 -0500

    stores

--- starscape-frontend/src/stores/counter.js
+++ starscape-frontend/src/stores/counter.js
@@ -1,12 +1,9 @@
 import { ref, computed } from 'vue'
 import { defineStore } from 'pinia'
 
-export const useCounterStore = defineStore('counter', () => {
-  const count = ref(0)
-  const doubleCount = computed(() => count.value * 2)
-  function increment() {
-    count.value++
-  }
+export const shipStore = defineStore('shipStore', () => {
+  let currentShip = ref() // use for selecting ships
+  let currentTurret = ref() // use for selecting turrets
 
-  return { count, doubleCount, increment }
+  return { currentShip, currentTurret }
 })
