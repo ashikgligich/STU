ee04a40326d9847aa5dd3da424e357458c28f7cb
LeoDwi <leod13579@gmail.com>
2024-02-13 20:18:49 -0500

    eeee

--- starscape-frontend/src/views/AboutView.vue
+++ starscape-frontend/src/views/AboutView.vue
@@ -1,11 +1,13 @@
 <template>
   <div>
-    <div class="about" v-if="gotData">
+    <div class="about">
       <h1>This is an about page</h1>
-      <label for="cars">Choose a car:</label>
-      <select name v-for="ship in ships.value" :key="ship">
-        <option value="ship.ship">{{ ship.ship }}</option>
-      </select>
+      <form v-if="gotData">
+        <label for="cars">Choose a car:</label>
+        <select name v-for="ship in ships.value" :key="ship" id="cars">
+          <option value="ship.ship">{{ ship.ship }}</option>
+        </select>
+      </form>
       <h4></h4>
     </div>
     <main>
@@ -32,8 +34,9 @@ let ships = ref()
 let gotData = ref(false)
 
 async function fetchShips() {
-  let url = 'https://api.sheety.co/9aee2b657f33b4b9cde173e45d295b27/sTarscape/ships'
-  const response = await fetch(url)
+  const response = await fetch(
+    'https://api.sheety.co/9aee2b657f33b4b9cde173e45d295b27/sTarscape/ships'
+  )
   const array = await response.json()
   ships.value = array
   gotData.value = true
