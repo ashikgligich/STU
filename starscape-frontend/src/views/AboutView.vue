f22c80693cb413f8acd4b9883d678f3700381c3a
ashikgligich <144455072+ashikgligich@users.noreply.github.com>
2024-02-13 20:00:04 -0500

    e
    e

--- starscape-frontend/src/views/AboutView.vue
+++ starscape-frontend/src/views/AboutView.vue
@@ -1,11 +1,20 @@
 <template>
   <div class="about">
-    <h1>This is an about page</h1>\
+    <h1>This is an about page</h1>
+    <label for="cars">Choose a car:</label>
+<select name v-for="ship in ships" :key="ship">
+<option value="ship.ship">{{ ship.ship }}</option>
+
+
+</select>
     <h4></h4>
   </div>
   <main>
     <MainModel />
   </main>
+
+
+
 </template>
 
 <style>
@@ -19,25 +28,34 @@
 </style>
 
 <script setup>
+import { onUnmounted, ref } from 'vue';
 import MainModel from '../components/MainModel.vue'
 
+function fetchShips() {
+    let url = 'https://api.sheety.co/9aee2b657f33b4b9cde173e45d295b27/sTarscape/ships';
+    return fetch(url)
+        .then((response) => response.json())
+        .then(json => {
+            // Do something with the data
+            console.log(json.ships);
+            return json.ships;
+            ships.value = json.ships;
+        });
+}
+let ships = ref();
+onUnmounted(() => {
+        fetchShips
 
-let url = 'https://api.sheety.co/9aee2b657f33b4b9cde173e45d295b27/sTarscape/ships';
-fetch(url)
-.then((response) => response.json())
-.then(json => {
-  // Do something with the data
-  console.log(json.ships);
-  let ships = json.ships;
+        
+    }); 
+fetchShips().then(ships => {
+    // Do something with the ships
+    
+
+
+    ships.forEach(ship => {
+        console.log(ship);
+    });
 
 });
-//output the array of ships to html page  
-    
-// ships.forEach(ship => {
-//   let card = document.createElement('div');
-//   card.className = 'card';
-//   card.innerHTML = ship.name;
-//   document.body.appendChild(card);
-// });
-
 </script>
