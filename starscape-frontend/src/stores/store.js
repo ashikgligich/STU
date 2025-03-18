ab9bfcbed87697653627b30c55a354460a7df324
LeoDwi <leod13579@gmail.com>
2025-03-17 21:29:32 -0400

    Update store.js

--- starscape-frontend/src/stores/store.js
+++ starscape-frontend/src/stores/store.js
@@ -1,7 +1,7 @@
 import { ref, computed } from 'vue'
 import { defineStore } from 'pinia'
 
-export const shipStore = defineStore('shipStore', () => {
+export default const shipStore = defineStore('shipStore', () => {
   let currentShip = ref() // use for selecting ships
   let currentTurret = ref() // use for selecting turrets
   let currentModule = ref() // use for selecting modules
